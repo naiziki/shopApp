cea34085ca523e036ddac160688b23e77792d2e2
naiziki <aavrnikh@gmail.com>
2021-09-13 22:34:52 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,17 @@ const cors = require("cors");
 const session = require("express-session");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
-const  connectDB  = require("./config/db");
-const productRouts= require('./routes/productRoutes');
+const connectDB = require("./config/db");
+const productRouts = require('./routes/productRoutes');
 const addproduct = require('./routes/addProductRouter');
 const admin = require('./routes/admin');
 const email = require('./routes/email');
 const category = require('./routes/category');
-const company =require('./routes/company');
+const company = require('./routes/company');
 const employer = require("./routes/employer")
+const path = require('path')
 
-const app  = express();
+const app = express();
 require('dotenv').config();
 
 
@@ -21,9 +22,9 @@ connectDB();
 app.use(fileUpload())
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}))  
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
 
 app.use(
   session({
@@ -35,19 +36,25 @@ app.use(
 );
 app.use(cookieParser(process.env.SESSION));
 app.use(passport.initialize());
-app.use(passport.session()); 
+app.use(passport.session());
 
 
-app.use('/api/prodcuts',productRouts);
-app.use('/api/addproducts',addproduct);
-app.use('/api/admin/', admin); 
-app.use('/api/email/send',email);
-app.use('/api/category',category);
-app.use('/api/company',company);
-app.use('/api/employer',employer);
+app.use('/api/prodcuts', productRouts);
+app.use('/api/addproducts', addproduct);
+app.use('/api/admin/', admin);
+app.use('/api/email/send', email);
+app.use('/api/category', category);
+app.use('/api/company', company);
+app.use('/api/employer', employer);
 
+if (process.env.NOD_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
 
-const port =  process.env.PORT; 
-app.listen(port,()=>{
-    console.log(`server running on port ${port}`)
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`server running on port ${port}`)
 })
